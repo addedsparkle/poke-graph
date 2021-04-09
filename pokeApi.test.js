7b668e0d0ec9ab17edd9d2354d116d1e5861c922
Jennifer Jensen <addedsparkle@gmail.com>
2021-04-09 16:23:04 +0100

    Add types that pokemon takes 2x damage from

--- pokeApi.test.js
+++ pokeApi.test.js
@@ -1,5 +1,6 @@
 const pokeApi = require("./pokeApi");
 const hawlucha = require("./test/fixtures/hawlucha");
+const normalType = require("./test/fixtures/normal-type");
 
 it("should fetch a list of pokemon", () => {
   expect(pokeApi.getPokemon()).resolves.toHaveLength(20);
@@ -8,3 +9,7 @@ it("should fetch a list of pokemon", () => {
 it("should fetch a single pokemon by name", () => {
   expect(pokeApi.getPokemonByName("hawlucha")).resolves.toEqual(hawlucha);
 });
+
+it('should fetch a resource by url', () => {
+  expect(pokeApi.getResource('https://pokeapi.co/api/v2/type/1/')).resolves.toEqual(normalType);
+})
