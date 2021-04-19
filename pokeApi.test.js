b256ad8b2568b606507441e84c4af371f30f70c4
Jennifer Jensen <addedsparkle@gmail.com>
2021-04-19 21:51:46 +0100

    Add RESTDataSource

--- pokeApi.test.js
+++ pokeApi.test.js
@@ -1,15 +1,24 @@
-const pokeApi = require("./pokeApi");
+const { PokeAPI } = require("./pokeApi");
 const hawlucha = require("./test/fixtures/hawlucha");
 const normalType = require("./test/fixtures/normal-type");
 
-it("should fetch a list of pokemon", () => {
-  return expect(pokeApi.getPokemon()).resolves.toHaveLength(20);
-});
+describe('The PokeApi',  () => {
 
-it("should fetch a single pokemon by name", () => {
-  return expect(pokeApi.getPokemonByName("hawlucha")).resolves.toEqual(hawlucha);
-});
+  let pokeApi;
 
-it('should fetch a resource by url', () => {
-  return expect(pokeApi.getResource('https://pokeapi.co/api/v2/type/1/')).resolves.toEqual(normalType);
-})
+  beforeAll( () => {
+    pokeApi = new PokeAPI();
+    pokeApi.initialize({});
+  })
+  it("should fetch a list of pokemon", () => {
+    return expect(pokeApi.getPokemon()).resolves.toHaveLength(20);
+  });
+
+  it("should fetch a single pokemon by name", () => {
+    return expect(pokeApi.getPokemonByName("hawlucha")).resolves.toEqual(hawlucha);
+  });
+
+  it('should fetch a resource by url', () => {
+    return expect(pokeApi.getResource('https://pokeapi.co/api/v2/type/1/')).resolves.toEqual(normalType);
+  })
+});
