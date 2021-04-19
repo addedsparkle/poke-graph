2b4109756bd783b6d2ce67350683ed34779b4b45
Jennifer Jensen <addedsparkle@gmail.com>
2021-04-19 20:14:23 +0100

    Add sprites to demonstrate n+1 issue

--- pokeApi.test.js
+++ pokeApi.test.js
@@ -3,13 +3,13 @@ const hawlucha = require("./test/fixtures/hawlucha");
 const normalType = require("./test/fixtures/normal-type");
 
 it("should fetch a list of pokemon", () => {
-  expect(pokeApi.getPokemon()).resolves.toHaveLength(20);
+  return expect(pokeApi.getPokemon()).resolves.toHaveLength(20);
 });
 
 it("should fetch a single pokemon by name", () => {
-  expect(pokeApi.getPokemonByName("hawlucha")).resolves.toEqual(hawlucha);
+  return expect(pokeApi.getPokemonByName("hawlucha")).resolves.toEqual(hawlucha);
 });
 
 it('should fetch a resource by url', () => {
-  expect(pokeApi.getResource('https://pokeapi.co/api/v2/type/1/')).resolves.toEqual(normalType);
+  return expect(pokeApi.getResource('https://pokeapi.co/api/v2/type/1/')).resolves.toEqual(normalType);
 })
