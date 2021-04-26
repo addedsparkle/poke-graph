2ed7ee748c4e437f98391fcb66a8295903ea69f6
addedsparkle <addedsparkle@gmail.com>
2021-04-26 20:39:31 +0100

    Recreate mutation branch

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-const { ApolloServer, gql, SchemaDirectiveVisitor } = require('apollo-server');
-const { defaultFieldResolver, GraphQLString } = require('graphql');
+const { ApolloServer, gql } = require('apollo-server');
 const { PokeAPI } = require('./pokeApi');
 const { TrainerAPI } = require('./trainerApi');
 
-class ifInVersionDirective extends SchemaDirectiveVisitor {
-  visitFieldDefinition(field) {
-    const { resolve = defaultFieldResolver } = field;
-    field.args.push({
-      name: 'version',
-      type: GraphQLString
-    });
-    field.resolve = async function (source, {version, ...otherArgs}, context, info) {
-      const pokemonList = await resolve.call(this, source, otherArgs, context, info);
-      return version ? pokemonList.filter( pokemon => pokemon.game_indices && pokemon.game_indices.find(gameIndex => gameIndex.version.name === version)) : pokemonList;
-    };
-  }
-};
-
 const typeDefs = gql`
-
-  directive @ifInVersion(version: String) on FIELD_DEFINITION
-
   type Pokemon {
     name: String
     id: Int
     types: [Type]
     url: String
     image: String
-    game_indices: [GameIndex] 
   }
 
-  type GameIndex {
-    game_index: Int
-    version: Version
-  }
-
-  type Version {
-    name: String
-    url: String
-  }
   type Type {
     name: String
     double_damage_from: [Type] 
@@ -47,7 +19,6 @@ const typeDefs = gql`
     half_damage_to: [Type]
     no_damage_from: [Type]
     no_damage_to: [Type]
-    pokemon: [Pokemon] @ifInVersion
   }
 
   type Query {
@@ -72,7 +43,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addPokemons(name: String, pokemon: [PokemonInput]): TrainerUpdateResponse
+    addTrainerPokemon(name: String, pokemon: [PokemonInput]): TrainerUpdateResponse
   }
 `;
 
@@ -80,9 +51,10 @@ const resolvers = {
   Query: {
     pokemons: (_source, _args, {dataSources}) => dataSources.pokeApi.getPokemon(),
     pokemon: (_source, {name}, {dataSources}) => dataSources.pokeApi.getPokemonByName(name),
+    trainer: (_source, {name}, {dataSources}) => dataSources.trainerApi.getTrainer(name),
   },
   Mutation: {
-    addPokemons: async (_source, {name, pokemon}, {dataSources}) => dataSources.trainerApi.addPokemon(name, pokemon),
+    addTrainerPokemon: (_source, {name, pokemon}, {dataSources}) => dataSources.trainerApi.addPokemon(name, pokemon),
   },
   Pokemon: {
     types: (_source, _args, {dataSources}) => Promise.all(_source.types.map(({type}) => dataSources.pokeApi.getResource(type.url))),
@@ -94,16 +66,15 @@ const resolvers = {
     half_damage_from: (_source, _args, {dataSources}) => Promise.all(_source.damage_relations.half_damage_from.map( type => dataSources.pokeApi.getResource(type.url))),
     half_damage_to: (_source, _args, {dataSources}) => Promise.all(_source.damage_relations.half_damage_to.map( type => dataSources.pokeApi.getResource(type.url))),
     no_damage_from: (_source, _args, {dataSources}) => Promise.all(_source.damage_relations.no_damage_from.map( type => dataSources.pokeApi.getResource(type.url))),
-    no_damage_to: (_source, _args, {dataSources}) => Promise.all(_source.damage_relations.no_damage_to.map( type => dataSources.pokeApi.getResource(type.url))),
-    pokemon: (_source, _args, {dataSources}) => Promise.all(_source.pokemon.map(({pokemon}) => dataSources.pokeApi.getResource(pokemon.url)))
+    no_damage_to: (_source, _args, {dataSources}) => Promise.all(_source.damage_relations.no_damage_to.map( type => dataSources.pokeApi.getResource(type.url)))
+  },
+  Trainer: {
+    pokemon: (_source,_args, {dataSources}) => Promise.all(_source.pokemon.map(pokemon => dataSources.pokeApi.getResource(pokemon.url))),
   }
 };
 
 const server = new ApolloServer({ 
   typeDefs,
-  schemaDirectives: {
-    ifInVersion: ifInVersionDirective
-  },
   resolvers, 
   dataSources: () => {
     return {
